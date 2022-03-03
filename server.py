cca4eba389ffa7d97fc6f2aceca2e7d4d45242e4
atiglla <99499241+atiglla@users.noreply.github.com>
2022-03-03 16:04:12 -0500

    second commit

--- server.py
+++ server.py
@@ -3,15 +3,19 @@ app=Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", times=0)
+    return render_template("index.html", times=1,color="aquamarine")
 
 @app.route("/play")
 def play ():
-    return render_template("index.html", times=3)
+    return render_template("index.html", times=3,color="aquamarine")
 
 @app.route("/play/<int:numero>")
 def varias (numero):
     return render_template("index.html", times=numero)
+
+@app.route("/play/<int:numero>/<y>")
+def varias2 (numero,y):
+    return render_template("index.html", times=numero, color=y)
 
 
 
